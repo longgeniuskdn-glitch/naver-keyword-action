d0fa00bf2c6ecef4310d5e75d4d1ad855739def3
longgeniuskdn-glitch <longgeniuskdn@gmail.com>
2026-07-26 17:46:21 +0900

    Export feedback engine API

---
+++ common_feedback/__init__.py
@@ -0,0 +1,17 @@
+from .engine import (
+    FeedbackEngine,
+    FeedbackEngineError,
+    FeedbackRecord,
+    InvalidMemoryPathError,
+    MODULE_PROFILES,
+    RuleConflictError,
+)
+
+__all__ = [
+    "FeedbackEngine",
+    "FeedbackEngineError",
+    "FeedbackRecord",
+    "InvalidMemoryPathError",
+    "MODULE_PROFILES",
+    "RuleConflictError",
+]
