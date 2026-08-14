248459861487d699acbed5196366451a28a07350
longgeniuskdn-glitch <longgeniuskdn@gmail.com>
2026-08-14 20:52:48 +0900

    Bump Naver Clip AutoSwipe to v0.3.3

--- naverclip_v030/app/build.gradle.kts
+++ naverclip_v030/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.myvision.naverclipautoswipe"
         minSdk = 30
         targetSdk = 30
-        versionCode = 8
-        versionName = "0.3.2"
+        versionCode = 9
+        versionName = "0.3.3"
     }
 
     buildTypes {
