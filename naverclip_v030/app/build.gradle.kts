26594fbf3ba8977008ae311fe4eeadc7a161657d
longgeniuskdn-glitch <longgeniuskdn@gmail.com>
2026-08-14 19:35:53 +0900

    Add v0.3.0 app build config

---
+++ naverclip_v030/app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.myvision.naverclipautoswipe"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.myvision.naverclipautoswipe"
+        minSdk = 30
+        targetSdk = 30
+        versionCode = 6
+        versionName = "0.3.0"
+    }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions { jvmTarget = "1.8" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.github.MuntashirAkon:libadb-android:3.1.0")
+    implementation("org.conscrypt:conscrypt-android:2.5.3")
+    implementation("com.github.MuntashirAkon:sun-security-android:1.1")
+}
